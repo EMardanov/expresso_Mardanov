07647ce01cf3789ea68f0304cb26e8fb628cfa72
EMardanov <emardanov@o365.comhard.de>
2024-10-24 01:44:39 +0200

    jwt_secret_key added

--- secrets.js
+++ secrets.js
@@ -2,7 +2,8 @@ const secrets = {
     db_server: 'localhost',
     db_username: 'nicle',
     db_password: 'beep',
-    db_database: 'project_db'
+    db_database: 'project_db',
+    jwt_secret_key: 'qwertzuioplkjhgfdsayxcvbnm'
 }
 
 export default secrets;
