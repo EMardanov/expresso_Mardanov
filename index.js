720ba75ed0f2614ba232de25a4330575f8a15fe4
EMardanov <emardanov@o365.comhard.de>
2024-10-24 01:46:31 +0200

    Authentification funktion added

--- index.js
+++ index.js
@@ -1,14 +1,43 @@
 import express from 'express';
 import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+import cookieParser from 'cookie-parser';
 import pool from './db.js';
+import secrets from './secrets.js';
 import { createPool } from 'mariadb';
 
 const app = express();
 
 // EJS als View-Engine einrichten
 app.set('view engine', 'ejs');
+// Route für View-Engine einrichten
 app.use(express.static('public'));
+//Middleware, um URL-encoded.Daten zu verarbeiten
 app.use(express.urlencoded({ extended: true }));
+// Cookie-Parser-Middleware verwenden
+app.use(cookieParser());
+
+app.get('/', (req, res) => {
+  const token = req.cookies['token'];
+  let loggedInUser = false;
+  if (token) {
+    // Token verifizieren und Benutzerdaten erhalten
+    jwt.verify(token, secrets.jwt_secret_key, (err, user) => {
+          if (err) {
+              // Token ungültig
+              console.log(err);
+          } else {
+              loggedInUser = user;
+          }
+    });
+  }
+  res.render('index',
+      { title: 'Willkommen',
+        message: 'Willkommen bei Ihrem ersten Express-Server!',
+        user: loggedInUser
+      }
+  );
+});
 
 // Route mit dynamischem Inhalt
 app.get('/', (req, res) => {
@@ -65,6 +94,38 @@ app.post('/register', async (req, res) => {
   } finally {
     conn.release();
   }
+});
+
+app.post('/login', async (req, res) => {
+  const { username, password } = req.body;
+  const conn = await pool.getConnection();
+  let user;
+
+  try {
+      user = await conn.query('SELECT * FROM users WHERE username = ?', [username]);
+  } catch (err) {
+      console.log(err);
+      return res.status(500).render('login', { title: 'Login', error: 'Fehler beim Login'});
+  } finally {
+    conn.release();
+  }
+
+  if (user && user.length === 0) {
+    return res.status(404).render('login', { title: 'Login', error: 'Benutzer nicht gefunden.' });
+  }
+
+  const isMatch = await bcrypt.compare(password, user[0].password_hash);
+  if (!isMatch) {
+    return res.status(403).render('login', { title: 'Login', error: 'Falsches Passwort'});
+  }
+  const token = jwt.sign({ username: user[0].username, name: user[0].name, email: user[0].email },
+                           secrets.jwt_secret_key,
+                           { expiresIn: '1h' });
+  res.cookie('token', token, { httpOnly: true }).redirect('/');
+});
+
+app.post('/logout', (req, res) => {
+  res.clearCookie('token').redirect('/');
 });
 
 // Server starten
